960951d69f8f1be8ff13e299368a6dd848e3cbe1
Мамедов Анвер Али-Бала <Mamed_kn64@itstep.edu.az>
2024-11-02 18:34:36 +0400

    small changes

--- food.client/src/App.js
+++ food.client/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'; 
 
 function App() {
+  const location = useLocation(); // Получаем текущий путь
+
+  // Определяем страницы, на которых не будет футера
+  const hideFooterPaths = ['/login', '/register'];
+
   return (
     <>
-    <header>
-      <Navbar />
-    </header>
-    <div className="App">
-      <Outlet />
-    </div>
+      <header>
+        <Navbar />
+      </header>
+      <div className="App">
+        <Outlet />
+      </div>
+      {/* Условное отображение футера */}
+      {!hideFooterPaths.includes(location.pathname) && <Footer />}
     </>
   );
 }
 
 export default App;
-  
---
+++ food.client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer-content">
+          <p>© 2024 Tasty Bytes®. Все права защищены.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
